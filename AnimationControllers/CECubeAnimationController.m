d54c2137da0726c5482f109399d365b6e90a713d
Hossein <Hossein.Karimy@gmail.com>
2015-12-14 00:05:44 -0500

    no message

--- AnimationControllers/CECubeAnimationController.m
+++ AnimationControllers/CECubeAnimationController.m
@@ -15,105 +15,123 @@
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-        self.cubeAnimationWay = CubeAnimationWayHorizontal;
-    }
-    return self;
+  self = [super init];
+  if (self) {
+    self.cubeAnimationWay = CubeAnimationWayHorizontal;
+  }
+  return self;
 }
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext fromVC:(UIViewController *)fromVC toVC:(UIViewController *)toVC fromView:(UIView *)fromView toView:(UIView *)toView
 {
-
-    //Calculate the direction
-    int dir = self.reverse ? 1 : -1;
+  //We create a content view for do the translate animation
+  UIView *generalContentView = [transitionContext containerView];
+  //Add the to- view
+  [generalContentView addSubview:toView];
   
-    //Create the differents 3D animations
-    CATransform3D viewFromTransform;
-    CATransform3D viewToTransform;
-
-    //We create a content view for do the translate animation
-    UIView *generalContentView = [transitionContext containerView];
-                                  
+  [self doAnim:fromView toV:toView duration:[self transitionDuration:transitionContext] target:self onComplete:@selector(doOnComplete:)];
+}
+// removes all the views other than the given view from the superview
+- (void)doAnim:(UIView*)fromV toV:(UIView*)toV duration:(NSTimeInterval)duration target:(id)target onComplete:(SEL)onComplete{
+  //Calculate the direction
+  int dir = self.reverse ? 1 : -1;
+  
+  //Create the differents 3D animations
+  CATransform3D viewFromTransform;
+  CATransform3D viewToTransform;
+  
+  //We create a content view for do the translate animation
+  UIView *generalContentView = [fromV superview];
+  
+  switch (self.cubeAnimationWay) {
+    case CubeAnimationWayHorizontal:
+      viewFromTransform = CATransform3DMakeRotation(dir*ROTATION_ANGLE, 0.0, 1.0, 0.0);
+      viewToTransform = CATransform3DMakeRotation(-dir*ROTATION_ANGLE, 0.0, 1.0, 0.0);
+      [toV.layer setAnchorPoint:CGPointMake(dir==1?0:1, 0.5)];
+      [fromV.layer setAnchorPoint:CGPointMake(dir==1?1:0, 0.5)];
+      
+      [generalContentView setTransform:CGAffineTransformMakeTranslation(dir*(generalContentView.frame.size.width)/2.0, 0)];
+      break;
+      
+    case CubeAnimationWayVertical:
+      viewFromTransform = CATransform3DMakeRotation(-dir*ROTATION_ANGLE, 1.0, 0.0, 0.0);
+      viewToTransform = CATransform3DMakeRotation(dir*ROTATION_ANGLE, 1.0, 0.0, 0.0);
+      [toV.layer setAnchorPoint:CGPointMake(0.5, dir==1?0:1)];
+      [fromV.layer setAnchorPoint:CGPointMake(0.5, dir==1?1:0)];
+      
+      [generalContentView setTransform:CGAffineTransformMakeTranslation(0, dir*(generalContentView.frame.size.height)/2.0)];
+      break;
+      
+    default:
+      break;
+  }
+  
+  viewFromTransform.m34 = PERSPECTIVE;
+  viewToTransform.m34 = PERSPECTIVE;
+  
+  toV.layer.transform = viewToTransform;
+  
+  //Create the shadow
+  UIView *fromShadow = [self addOpacityToView:fromV withColor:[UIColor blackColor]];
+  UIView *toShadow = [self addOpacityToView:toV withColor:[UIColor blackColor]];
+  [fromShadow setAlpha:0.0];
+  [toShadow setAlpha:1.0];
+  
+  UIColor *bgColor = generalContentView.backgroundColor;
+  generalContentView.backgroundColor = [UIColor blackColor];
+  [UIView animateWithDuration:duration animations:^{
     switch (self.cubeAnimationWay) {
-        case CubeAnimationWayHorizontal:
-            viewFromTransform = CATransform3DMakeRotation(dir*ROTATION_ANGLE, 0.0, 1.0, 0.0);
-            viewToTransform = CATransform3DMakeRotation(-dir*ROTATION_ANGLE, 0.0, 1.0, 0.0);
-            [toView.layer setAnchorPoint:CGPointMake(dir==1?0:1, 0.5)];
-            [fromView.layer setAnchorPoint:CGPointMake(dir==1?1:0, 0.5)];
-            
-            [generalContentView setTransform:CGAffineTransformMakeTranslation(dir*(generalContentView.frame.size.width)/2.0, 0)];
-            break;
-            
-        case CubeAnimationWayVertical:
-            viewFromTransform = CATransform3DMakeRotation(-dir*ROTATION_ANGLE, 1.0, 0.0, 0.0);
-            viewToTransform = CATransform3DMakeRotation(dir*ROTATION_ANGLE, 1.0, 0.0, 0.0);
-            [toView.layer setAnchorPoint:CGPointMake(0.5, dir==1?0:1)];
-            [fromView.layer setAnchorPoint:CGPointMake(0.5, dir==1?1:0)];
-            
-            [generalContentView setTransform:CGAffineTransformMakeTranslation(0, dir*(generalContentView.frame.size.height)/2.0)];
-            break;
-            
-        default:
-            break;
+      case CubeAnimationWayHorizontal:
+        [generalContentView setTransform:CGAffineTransformMakeTranslation(-dir*generalContentView.frame.size.width/2.0, 0)];
+        break;
+        
+      case CubeAnimationWayVertical:
+        [generalContentView setTransform:CGAffineTransformMakeTranslation(0, -dir*(generalContentView.frame.size.height)/2.0)];
+        break;
+        
+      default:
+        break;
     }
-
-    viewFromTransform.m34 = PERSPECTIVE;
-    viewToTransform.m34 = PERSPECTIVE;
-
-    toView.layer.transform = viewToTransform;
     
-    //Create the shadow
-    UIView *fromShadow = [self addOpacityToView:fromView withColor:[UIColor blackColor]];
-    UIView *toShadow = [self addOpacityToView:toView withColor:[UIColor blackColor]];
-    [fromShadow setAlpha:0.0];
-    [toShadow setAlpha:1.0];
+    fromV.layer.transform = viewFromTransform;
+    toV.layer.transform = CATransform3DIdentity;
     
-    //Add the to- view
-    [generalContentView addSubview:toView];
+    [fromShadow setAlpha:1.0];
+    [toShadow setAlpha:0.0];
     
-    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
-        switch (self.cubeAnimationWay) {
-            case CubeAnimationWayHorizontal:
-                [generalContentView setTransform:CGAffineTransformMakeTranslation(-dir*generalContentView.frame.size.width/2.0, 0)];
-                break;
-                
-            case CubeAnimationWayVertical:
-                [generalContentView setTransform:CGAffineTransformMakeTranslation(0, -dir*(generalContentView.frame.size.height)/2.0)];
-                break;
-                
-            default:
-                break;
-        }
-        
-        fromView.layer.transform = viewFromTransform;
-        toView.layer.transform = CATransform3DIdentity;
-        
-        [fromShadow setAlpha:1.0];
-        [toShadow setAlpha:0.0];
-        
-    } completion:^(BOOL finished) {
-        
-        //Set the final position of every elements transformed
-        [generalContentView setTransform:CGAffineTransformIdentity];
-        fromView.layer.transform = CATransform3DIdentity;
-        toView.layer.transform = CATransform3DIdentity;
-        [fromView.layer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
-        [toView.layer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
-        
-        [fromShadow removeFromSuperview];
-        [toShadow removeFromSuperview];
-        
-        if ([transitionContext transitionWasCancelled]) {
-            [toView removeFromSuperview];
-        } else {
-            [fromView removeFromSuperview];
-        }
-        
-        // inform the context of completion
-        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
-        
-    }];
+  } completion:^(BOOL finished) {
+    
+    //Set the final position of every elements transformed
+    [generalContentView setTransform:CGAffineTransformIdentity];
+    fromV.layer.transform = CATransform3DIdentity;
+    toV.layer.transform = CATransform3DIdentity;
+    [fromV.layer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
+    [toV.layer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
+    
+    [fromShadow removeFromSuperview];
+    [toShadow removeFromSuperview];
+    
+    // remove all the temporary views
+    if ((self.transitionContext == nil && !finished) ||
+        (self.transitionContext != nil && [self.transitionContext transitionWasCancelled])) {
+      [toV removeFromSuperview];
+    } else {
+      [fromV removeFromSuperview];
+    }
+    
+    generalContentView.backgroundColor = bgColor;
+    
+    if ([target respondsToSelector:onComplete]) {
+      ((void (*)(id, SEL, BOOL))[target methodForSelector:onComplete])
+      (target, onComplete, finished);
+    }
+    
+  }];
+}
+// animation finished
+-(void)doOnComplete:(BOOL)isDone{
+  //inform the context of completion
+  [self.transitionContext completeTransition:![self.transitionContext transitionWasCancelled]];
 }
 
 - (UIView *)addOpacityToView:(UIView *) view withColor:(UIColor *)theColor
